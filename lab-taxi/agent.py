b7adc4d05c273b44119280496ebc0e24df57f39a
aanrran <aanrran@gmail.com>
2020-09-13 13:03:54 -0700

    Add files via upload

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -12,8 +12,32 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        
+        self.epsilon = 1.0
+        self.alpha = 0.3
+        self.gamma = 0.9
+        
+        self.state = 0
+        self.action = 0
+        
+        self.eps = 0.005
+        self.i_episode = 0
+        
+        self.q_learning = True
+        
+    def epsilon_greedy_probs(self, state):
+        """ obtains the action probabilities corresponding to epsilon-greedy policy """
 
-    def select_action(self, state):
+        if self.q_learning == True:
+            self.epsilon = max(1.0/self.i_episode, self.eps)
+        else:
+            self.epsilon = self.eps
+            
+        policy_s = np.ones(self.nA) * self.epsilon / self.nA
+        policy_s[np.argmax(state)] = 1 - self.epsilon + (self.epsilon / self.nA)
+        return policy_s
+
+    def select_action(self, state, i_episode):
         """ Given the state, select an action.
 
         Params
@@ -24,9 +48,13 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        self.i_episode = i_episode
+        policy = self.epsilon_greedy_probs(self.Q[state])
+        self.action = np.random.choice(np.arange(self.nA), p=policy) if state in self.Q else np.random.choice(self.nA)
+        self.state = state
+        return self.action
 
-    def step(self, state, action, reward, next_state, done):
+    def step(self, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
         Params
@@ -37,4 +65,11 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        Qsa = self.Q[self.state][self.action]
+        Qs_next = self.Q[next_state]
+        if self.q_learning == True:
+            self.Q[self.state][self.action] = Qsa + (self.alpha * (reward + (self.gamma * np.max(Qs_next)) - Qsa)) 
+        else:
+            policy = self.epsilon_greedy_probs(self.Q[next_state])
+            self.Q[self.state][self.action] = Qsa + (self.alpha * (reward + (self.gamma * np.dot(policy, Qs_next)) - Qsa))
+        
